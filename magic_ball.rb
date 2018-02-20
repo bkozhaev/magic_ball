ce99a18b548ad68a35fe32c91b6e921f556aea3b
baur <bkozhaev@gmail.com>
2018-02-20 11:10:04 +0400

    Initial commit

---
+++ magic_ball.rb
@@ -0,0 +1,19 @@
+
+require_relative 'answers.rb'
+require_relative 'greetings.rb'
+
+# XXX/ Этот код необходим только при использовании русских букв на Windows
+if (Gem.win_platform?)
+  Encoding.default_external = Encoding.find(Encoding.locale_charmap)
+  Encoding.default_internal = __ENCODING__
+
+  [STDIN, STDOUT].each do |io|
+    io.set_encoding(Encoding.default_external, Encoding.default_internal)
+  end
+end
+# /XXX
+
+puts GREETINGS.sample
+sleep(2)
+puts
+puts ANSWERS.sample
